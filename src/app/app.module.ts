4979f34c6a9c8606188e2ee5f4e4b5f7ef28540e
Rajkumar <rajkumar.akoju@gmail.com>
2018-02-03 20:58:54 +0530

    Side Nav and Routing Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
+
+import {AppRoutingModule} from './app-routing/app-routing.module'
 
 
 import { AppComponent } from './app.component';
 
+import 'hammerjs';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
