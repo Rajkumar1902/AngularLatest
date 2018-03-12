5a928d2114056a94effe00888e1d9544b54bf4bc
Rajkumar <rajkumar.akoju@gmail.com>
2018-03-12 11:03:16 +0530

    header button style issue

--- src/app/shared/TempData/locations.ts
+++ src/app/shared/TempData/locations.ts
@@ -4,21 +4,25 @@ import { Location} from '../location';
 export const LOCATIONS: Location[] = [
   {
     locationCode: 'HYD',
+    locationName: 'Hyderabad',
     city: 'Hyderabad',
     country: 'INDIA',
   },
   {
     locationCode: 'BLR',
+    locationName: 'Bangalore',
     city: 'Bangalore',
     country: 'INDIA',
   },
   {
     locationCode: 'NZB',
+    locationName: 'Nizamabad',
     city: 'Nizamabad',
     country: 'INDIA',
   },
   {
     locationCode: 'MUM',
+    locationName: 'Mumbai',
     city: 'Mumbai',
     country: 'INDIA',
   }
